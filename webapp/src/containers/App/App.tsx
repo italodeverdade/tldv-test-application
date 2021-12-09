108e220feaefc59c4f7d376b73db9c7cddc84f10
Italo Brasil <italo@brasilstudio.co>
2021-12-09 16:09:21 -0300

    use typescript paths instead relative paths

--- webapp/src/containers/App/App.tsx
+++ webapp/src/containers/App/App.tsx
@@ -1,5 +1,5 @@
-import Button from '@webapp/components/Button';
 import React from 'react';
+import Button from '@webapp/components/Button';
 
 function App() {
   return (
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import App from '@webapp/containers/App';
+import reportWebVitals from '@webapp/reportWebVitals';
+import '@webapp/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
