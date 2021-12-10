ec24cf1c904c1471df801d63b303958ade149643
Italo Brasil <italo@brasilstudio.co>
2021-12-10 04:20:35 -0300

    add button spinner (loading state)

--- webapp/src/components/Button/Button.tsx
+++ webapp/src/components/Button/Button.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.types";
+import animation from "./animation.json";
+import Lottie from "lottie-react-web";
 
-const Button: React.FC<ButtonProps> = ({ label, ...rest }) => {
+const Button: React.FC<ButtonProps> = ({ label, loading, ...rest }) => {
   return (
     <button
       data-testid="Button::Button"
       {...rest}
       onClick={() => {
-        if (rest.onClick) {
+        if (rest.onClick && !loading) {
           rest.onClick();
         }
       }}
       className={styles.button}
     >
-      {label && <span data-testid="Button::Label">{label}</span>}
+      {!loading && label && <span data-testid="Button::Label">{label}</span>}
+      {loading && (
+        <Lottie
+          data-testid="Button::Animation"
+          width={24}
+          height={24}
+          options={{
+            animationData: animation,
+          }}
+        />
+      )}
     </button>
   );
 };
--- webapp/src/components/Button/Button.types.ts
+++ webapp/src/components/Button/Button.types.ts
@@ -1,4 +1,5 @@
 export type ButtonProps = {
     label: string;
     onClick?: () => void;
+    loading?: boolean;
 };
